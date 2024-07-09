2ea228ca1cf017116d57ede8c9924f9f5bd3af7b
pcarelli <porterkcarelli@gmail.com>
2024-07-09 13:29:19 -0500

    unit conversions

--- strava_portfolio/src/Card.jsx
+++ strava_portfolio/src/Card.jsx
@@ -5,9 +5,49 @@ export default function Card(props){
     const options = {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric'}
     const rawStartDate = new Date(props.start_date)
 
+    function decimalToMinutePace(decimalSpeed){
+        let secs = Math.round((decimalSpeed-Math.floor(decimalSpeed))*60)
+        if (secs < 10){
+            secs = `0${secs}`
+        } else {
+            secs = secs
+        }
+        return `${Math.floor(decimalSpeed)}:${secs}`
+    }
+    
+    function convertSwim(speed){
+        const decimalPace = 100*Math.round((1/(speed*60))*10000)/10000
+        return decimalToMinutePace(decimalPace)
+    }
+
+    function convertRun(speed){
+        const decimalPace = Math.round((1/(speed*60))*1609*100)/100
+        return decimalToMinutePace(decimalPace)
+    }
+
+    function convertBike(speed){
+        const decimalPace = Math.round(((speed*60*60)/1609)*10)/10
+        return decimalPace
+    }
+
+    let pace
+
+    if(props.sport_type === "Swim"){
+        pace = `${convertSwim(props.average_speed)} min/100m`
+    } else if(props.sport_type === "Run"){
+        pace = `${convertRun(props.average_speed)} /mi`
+    } else if (props.sport_type === "Ride"){
+        pace = `${convertBike(props.average_speed)} mi/h`
+    }
+
     return (
         <div className="card">
-            <h2>{props.name} - {rawStartDate.toLocaleDateString("en-US", options)}</h2>
+            <h2>{props.name}</h2>
+            <div className="activity-details">
+                <p><span className="grey">Date:</span> {rawStartDate.toLocaleDateString("en-US", options)}</p>
+                <p><span className="grey">Type:</span> {props.sport_type}</p>
+                <p><span className="grey">Pace:</span> {pace}</p>
+            </div>
         </div>
     )
 }
