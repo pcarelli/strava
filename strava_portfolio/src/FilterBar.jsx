817644c49baff4e2084b1e0d36940b5ed8aa49a8
pcarelli <porterkcarelli@gmail.com>
2024-08-24 08:31:51 -0500

    rmv on click

--- strava_portfolio/src/FilterBar.jsx
+++ strava_portfolio/src/FilterBar.jsx
@@ -3,9 +3,9 @@ import React from "react"
 export default function FilterBar(props){
     return (
         <div className='filterBar'>
-          <button className='chiclet chiclet-swim' onClick={props.onClick}>Swim</button>
-          <button className='chiclet chiclet-bike' onClick={props.onClick}>Ride</button>
-          <button className='chiclet chiclet-run' onClick={props.onClick}>Run</button>
+          <button className='chiclet chiclet-swim'>Swim</button>
+          <button className='chiclet chiclet-bike'>Ride</button>
+          <button className='chiclet chiclet-run'>Run</button>
         </div>
     )
 }
