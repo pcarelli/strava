2ebf15ec4c2bdbcbd692d0fc462b411091663854
pcarelli <porterkcarelli@gmail.com>
2024-08-30 13:42:29 -0500

    login

--- strava_portfolio/src/App.jsx
+++ strava_portfolio/src/App.jsx
@@ -5,38 +5,6 @@ import Layout from './components/Layout.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App(){
-
-  const [stravaData, setStravaData] = React.useState([])
-
-  // React.useEffect(() => {
-  //   fetch("https://www.strava.com/oauth/token", {
-  //     mode: "no-cors",
-  //     method: "POST",
-  //     body: JSON.stringify({
-  //       client_id: import.meta.env.VITE_CLIENT_ID,
-  //       client_secret: import.meta.env.VITE_CLIENT_SECRET,
-  //       grant_type: "refresh_token",
-  //       refresh_token: import.meta.env.VITE_REFRESH_TOKEN
-  //     }),
-  //     headers: {
-  //       "Content-type": "application/json; charset=UTF-8"
-  //     }
-  //   })
-  //     .then(res => res.json())
-  //     .then(data => getActivities(data))
-  // }, [])
-
-  // function getActivities(data){
-  //   fetch("https://www.strava.com/api/v3/athlete/activities?per_page=200", {
-  //     method: "GET",
-  //     headers: {
-  //       "Authorization": `Bearer ${data.access_token}`
-  //     }
-  //   })
-  //     .then(res => res.json())
-  //     .then(data => setStravaData(data))
-  // }
-
   return (
     <BrowserRouter>
       <Routes>
--- strava_portfolio/src/pages/Home.jsx
+++ strava_portfolio/src/pages/Home.jsx
@@ -1,5 +1,54 @@
+import React from "react"
+import { Link, NavLink, useParams } from "react-router-dom"
+
 export default function Home(){
+    const [stravaData, setStravaData] = React.useState([])
+
+    const params = useParams()
+    console.log(params)
+
+    const clientID = "94243"
+    const clientSecret = "813838c6d34960ef53e23a5eb88b6c03b49883ce"
+    const refreshToken = "0884d1b71a828ff826ceed04cb4aceef23aae9f8"
+
+    function getAccessToken(){
+        fetch("https://www.strava.com/oauth/token", {
+            method: "POST",
+            body: JSON.stringify({
+              client_id: clientID,
+              client_secret: clientSecret,
+              grant_type: "refresh_token",
+              refresh_token: refreshToken
+            }),
+            headers: {
+              "Content-type": "application/json; charset=UTF-8"
+            }
+          })
+            .then(res => res.json())
+            .then(data => getActivities(data))
+    }
+
+    function getActivities(data){
+        fetch("https://www.strava.com/api/v3/athlete/activities?per_page=200", {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${data.access_token}`
+          }
+        })
+          .then(res => res.json())
+          .then(data => setStravaData(data))
+      }
+
+    const displayData = stravaData
+    console.log(displayData)
+
+
+
     return (
-        <h3>This is the home page</h3>
+        <>
+            <h3>{`Your client ID is ${clientID}`}</h3>
+            <Link to={`https://www.strava.com/oauth/authorize?client_id=${clientID}&redirect_uri=http://localhost:5173/&response_type=code&scope=read_all&activities=real_all`}>Authorize</Link>
+            <button onClick={() => getAccessToken()}>Get Strava data</button>
+        </>
     )
 }
