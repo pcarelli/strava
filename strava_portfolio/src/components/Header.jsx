4fd35b37aa4242ba658cc6f2235f3b63ce741858
pcarelli <porterkcarelli@gmail.com>
2024-08-30 12:47:56 -0500

    login

--- strava_portfolio/src/components/Header.jsx
+++ strava_portfolio/src/components/Header.jsx
@@ -1,13 +1,19 @@
 import React from "react"
 import { Link, NavLink } from "react-router-dom"
 
-export default function Header(props){
+export default function Header(){
+    const activeStyles = {
+        textDecoration: "underline",
+        fontWeight: "500",
+        color: "#E9D7C3"
+    }
+
     return (
         <header className="header">
             <Link to="/" className="h3">Frag|ment</Link>
             <div className="pages">
-                <NavLink to="/" className="link">Home</NavLink>
-                <NavLink to="login" className="link">Login</NavLink>
+                <NavLink to="/" className="link" style={({isActive}) => isActive ? activeStyles: null}>Home</NavLink>
+                <NavLink to="login" className="link" style={({isActive}) => isActive ? activeStyles: null}>Login</NavLink>
             </div>
         </header>
     )
--- strava_portfolio/src/pages/Login.jsx
+++ strava_portfolio/src/pages/Login.jsx
@@ -1,5 +1,42 @@
-export default function Home(){
+import React from "react"
+
+export default function Login(){
+    const [loginFormData, setLoginFormData] = React.useState({email: "", password: ""})
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(loginFormData)
+    }
+
+    function handleChange(e){
+        const {name, value} = e.target
+        setLoginFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
     return (
-        <h3>This is the login page</h3>
+        <div className="login-container">
+            <h3 className="login-title">Login</h3>
+            <form onSubmit={handleSubmit} className="login-form">
+                <input
+                    name="email"
+                    onChange={handleChange}
+                    type="email"
+                    placeholder="Email"
+                    value={loginFormData.email}
+                />
+                <input
+                    name="password"
+                    onChange={handleChange}
+                    type="password"
+                    placeholder="Password"
+                    value={loginFormData.password}
+                />
+                <button>Log in</button>
+            </form>
+            <span className="bottom-text">Don't have an account? <span className="bold underline">Create one</span></span>
+        </div>
     )
 }
